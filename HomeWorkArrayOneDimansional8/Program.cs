5fe131efa12db8fa73671febccb5a7f16ebb3d38
nikitaromanov <romanov_nikita@hotmail.com>
2020-10-27 20:28:26 +0300

    * Program.cs: * Program.cs: * HomeWorkArrayOneDimansional8.csproj: * HomeWorkArrayOneDimansional9.csproj:
    * HomeWorkArrayOneDimansional.sln: Готовая домашняя работа занятие 3
      Одномерные массивы задача 11-12

---
+++ HomeWorkArrayOneDimansional8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+//Отсортировать массив по возрастанию одним из способов:  пузырьком(Bubble),
+//выбором (Select) или вставками (Insert)) 
+
+namespace HomeWorkArrayOneDimansional8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array = new int [10];
+            int item;
+            Random random = new Random();
+
+            Console.WriteLine($"Неотсортированный массив: ");
+
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Array[i] = random.Next(1, 20);
+                Console.Write($"{Array[i]} ,");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Пузырьковая сортировка:");
+
+            for (int i = 0; i < Array.Length; i++)
+            {
+                for (int j = i + 1; j < Array.Length; j++)
+                {
+                    if (Array[i] > Array[j])
+                    {
+                        item = Array[i];
+                        Array[i] = Array[j];
+                        Array[j] = item;
+                    }
+                }
+                Console.Write($"{Array[i]}, ");
+            }
+        }
+    }
+}
---
+++ HomeWorkArrayOneDimansional9/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+// Отсортировать массив по убыванию одним из способов,
+// (отличным от способа в 9-м задании) :  пузырьком(Bubble), выбором (Select)
+// или вставками (Insert))
+
+namespace HomeWorkArrayOneDimansional9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array = new int [6];
+            Random random = new Random();
+            int item;
+
+            Console.WriteLine("Несортированный массив: ");
+
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Array[i] = random.Next(1, 20);
+                Console.Write($"{Array[i]}, ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Отсортированный массив: ");
+
+            for (int i = 0; i < Array.Length; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < Array.Length; j++)
+                {
+                    if (Array[j] < Array[min])
+                    {
+                        min = j;
+                    }
+                }
+                item = Array[min];
+                Array[min] = Array[i];
+                Array[i] = item;
+                Console.Write($"{Array[i]}, ");
+            }
+        }
+    }
+}
